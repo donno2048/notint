44aa95cfeffa5b7059aada0b7aa9644d95d671f9
Elisha Hollander <just4now666666@gmail.com>
2021-04-07 18:54:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 setup(
     name='notint',
-    version='1.0.0',
+    version='1.0.1',
     description='This is not an integer',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
